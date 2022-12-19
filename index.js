17ee641b7f549a3e5d5b45f1ecd50127a045d1df
nesricha <109212150+nesricha@users.noreply.github.com>
2022-12-19 13:10:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").textContent = "🚩Player 1 Wins!";
+  document.querySelector("h2").textContent = "🚩Player 1 Wins!";
 } else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").textContent = "Player 2 Wins!🚩";
+  document.querySelector("h2").textContent = "Player 2 Wins!🚩";
 } else {
-  document.querySelector("h1").textContent = "Draw!";
+  document.querySelector("h2").textContent = "Draw!";
 }
